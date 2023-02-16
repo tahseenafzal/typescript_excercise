0893f413e779116fb62bda775e0379be43ac8f01
Tehseen Afzal <abuashja@gmail.com>
2023-02-17 00:14:00 +0500

    Exercise 7 and 10
    Print math operations add, substract, divide, and multiply and add commit

---
+++ ex_07/index.ts
@@ -0,0 +1,8 @@
+// Tehseen Afzal
+// 16-Feb-2023
+// I have print using this program number 8 using addition, substraction, division and multiplication.
+
+console.log(5 + 3);
+console.log(10 - 2);
+console.log(2 * 4);
+console.log(40 / 5);
