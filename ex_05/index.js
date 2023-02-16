1a25e0188b8249ebce9b8a711c917fbff2e3dbf9
Tehseen Afzal <abuashja@gmail.com>
2023-02-17 00:07:18 +0500

    Exercise 5 Famous Person Quote with variable
    Store famous person name in a variable then concate the quote and variable value with template literal then print.

---
+++ ex_05/index.js
@@ -0,0 +1,3 @@
+"use strict";
+let famousPerson = "Albert Einstein";
+console.log(`${famousPerson} once said, “A person who never made a mistake never tried anything new.”`);
---
+++ ex_05/index.ts
@@ -0,0 +1,5 @@
+let famousPerson: string = "Albert Einstein";
+
+console.log(
+  `${famousPerson} once said, “A person who never made a mistake never tried anything new.”`
+);
